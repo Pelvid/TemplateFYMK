bbb5cff3040677aebad66954c4630805179b57d8
davidgiwa8 <davidgiwa8@gmail.com>
2022-02-21 17:23:12 -0800

    Add project files.

---
+++ TemplateFYMK/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TemplateFYMK.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult adminalert()
+        {
+            return View();
+        }
+        public ActionResult admincalender()
+        {
+            return View();
+        }
+        public ActionResult admindashboard()
+        {
+            return View();
+        }
+        public ActionResult adminmatch()
+        {
+            return View();
+        }
+        public ActionResult adminmentee()
+        {
+            return View();
+        }
+        public ActionResult adminmenteereview()
+        {
+            return View();
+        }
+        public ActionResult adminmentor()
+        {
+            return View();
+        }
+        public ActionResult adminmentorreview()
+        {
+            return View();
+        }
+
+        public ActionResult adminprofile()
+        {
+            return View();
+        }
+        public ActionResult adminprogram()
+        {
+            return View();
+        }
+        public ActionResult adminsettings()
+        {
+            return View();
+        }
+        public ActionResult chat()
+        {
+            return View();
+        }
+        public ActionResult helpcentre()
+        {
+            return View();
+        }
+        public ActionResult sidebar()
+        {
+            return View();
+        }
+    }
+}
